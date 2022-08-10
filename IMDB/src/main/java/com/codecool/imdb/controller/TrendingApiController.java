670a8757c56978c6d8cd9b0b15ab7f93dd9e997e
Nandorrr <87696275+Nandorrr@users.noreply.github.com>
2022-08-10 11:14:10 +0200

    add project skeleton

---
+++ IMDB/src/main/java/com/codecool/imdb/controller/TrendingApiController.java
@@ -0,0 +1,9 @@
+package com.codecool.imdb.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TrendingApiController {
+
+    //TO DO
+}
---
+++ IMDB/src/main/java/com/codecool/imdb/data/dao/UserDao.java
@@ -0,0 +1,4 @@
+package com.codecool.imdb.data.dao;
+
+public class UserDao {
+}
