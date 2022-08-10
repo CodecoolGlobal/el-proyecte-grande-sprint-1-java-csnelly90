c5b18fe0e74617ab72fb6a9c15b760f42fe41a64
Nandorrr <87696275+Nandorrr@users.noreply.github.com>
2022-08-10 11:02:13 +0200

    add initialized project

---
+++ IMDB/src/test/java/com/codecool/imdb/ImdbTests.java
@@ -0,0 +1,13 @@
+package com.codecool.imdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImdbTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
