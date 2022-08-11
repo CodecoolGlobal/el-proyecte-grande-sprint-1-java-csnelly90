3130ca1642bfebcb0cb43e1963415b4fc3c7b31e
eagle <sasbalazs05@gmail.com>
2022-08-11 10:52:02 +0200

    implement Artist class

--- IMDB/src/main/java/com/codecool/imdb/model/Artist.java
+++ IMDB/src/main/java/com/codecool/imdb/model/Artist.java
@@ -1,4 +1,21 @@
 package com.codecool.imdb.model;
 
 public class Artist {
+
+    private final String name;
+
+    private final String genre;
+
+    public Artist(String name, String genre) {
+        this.name = name;
+        this.genre = genre;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
 }
