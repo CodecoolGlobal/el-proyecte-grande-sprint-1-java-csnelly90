c70eaa8b668688ab902a43bb9cdf89c5b59e39ad
eagle <sasbalazs05@gmail.com>
2022-08-25 09:33:45 +0200

    show artists name on main page

--- frontend/src/routes/HomePage.js
+++ frontend/src/routes/HomePage.js
@@ -1,9 +1,26 @@
-import React from "react";
+import {useState, useEffect} from "react";
+import {dataHandler} from "../data/DataHandler";
 
 function HomePage() {
+    let [artists, setArtists] = useState(null);
+    let [albums, setAlbums] = useState(null);
+
+    useEffect(() => {
+        async function getData() {
+            let response = await dataHandler.apiGet("/api/artists");
+            setArtists(response);
+        }
+        getData();
+    }, []);
+
     return (
-        <div>
-            <h2>HomePage Content</h2>
+        <div class="top-artists">
+            {artists &&
+                artists.map(({ name, genre }) => (
+                    <div class="artist" key={name}>
+                        <h3>{name}</h3>
+                    </div>
+                ))}
         </div>
     );
 }
