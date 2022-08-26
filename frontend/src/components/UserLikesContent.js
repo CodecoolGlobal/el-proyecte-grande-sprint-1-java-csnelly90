7dcc035cfe6e5dda88f8280c36826f6cc3cda0c8
nelli <csnelly90@gmail.com>
2022-08-26 02:38:52 +0200

    implement fetch of liked content in UserLikesContent.js

--- frontend/src/components/UserLikesContent.js
+++ frontend/src/components/UserLikesContent.js
@@ -1,19 +1,52 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../UserPage.css"
+import {dataHandler} from "../data/DataHandler";
+
+function LikedContent(props) {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await dataHandler.apiGet(`/api/artist/${props.artistId}/`);
+            setData(response);
+        };
+
+        fetchData();
+    }, [props.id]);
+
+    if (data) {
+        return (
+            <div className="flex-row flex-space-between">
+                <div className="flex-col">
+                    <div id="user-likes-title">
+                        <h2>{data.name}</h2>
+                    </div>
+                    {/*<div><h1>{data.name}</h1></div>*/}
+                    <div className="justified-text">
+                        <p>{data.blurbs}</p>
+                    </div>
+                </div>
+                <div>
+                    <img src={data.image} alt=""/>
+                </div>
+            </div>
+    );
+    } else {
+        return null;
+    }
+}
 
 function UserLikesContent(props) {
+    const likedContent = props.likedArtists.map((artistId) =>
+        <div key={artistId} className={props.toggleState === 1 ? "likes-content active-content" : "likes-content"}>
+            <LikedContent artistId={artistId} />
+        </div>
+    );
+
     return (
         <div id="user-likes-content-container">
-            {/*<div id="user-likes-title">*/}
-            {/*    <h2>Liked artists</h2>*/}
-            {/*</div>*/}
-            <div
-                className={props.toggleState === 1 ? "likes-content active-content" : "likes-content"}
-            >
-                <p>
-                    Lorem ipsum
-                </p>
-            </div>
+
+            {likedContent}
 
             <div
                 className={props.toggleState === 2 ? "likes-content active-content" : "likes-content"}
