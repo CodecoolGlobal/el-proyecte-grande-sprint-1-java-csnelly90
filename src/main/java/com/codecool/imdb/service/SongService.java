537a58d2b1a1388ff432bacb9730107b652334db
nelli <csnelly90@gmail.com>
2022-09-06 14:25:13 +0200

    move RestTemplate to field in SongService

--- src/main/java/com/codecool/imdb/service/SongService.java
+++ src/main/java/com/codecool/imdb/service/SongService.java
@@ -1,7 +1,13 @@
 package com.codecool.imdb.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class SongService {
+
+    @Value("${api.key}")
+    private String apiKey;
+    private final RestTemplate restTemplate = new RestTemplate();
 }
