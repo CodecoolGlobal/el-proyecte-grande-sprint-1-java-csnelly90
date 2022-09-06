1a992fce92cedbd7d2c9d654262e173c71436a91
nelli <csnelly90@gmail.com>
2022-09-06 14:43:57 +0200

    refactor ArtistService

--- src/main/java/com/codecool/imdb/service/ArtistService.java
+++ src/main/java/com/codecool/imdb/service/ArtistService.java
@@ -8,7 +8,6 @@ import com.codecool.imdb.service.dtos.NapsterArtistResponse;
 import com.codecool.imdb.service.dtos.response.NapsterArtistCardDto;
 import com.codecool.imdb.domain.model.Artist;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -19,19 +18,26 @@ public class ArtistService {
     private String apiKey;
     private final RestTemplate restTemplate = new RestTemplate();
 
-    public Collection<Artist> getTopArtists(int limit) {
+    public Artist getArtist(String artistId) {
+        String url = "https://api.napster.com/v2.2/artists/" + artistId + "?apikey=" + apiKey;
+        var result = restTemplate.getForObject(url, NapsterArtistResponse.class);
+        return result.getArtists().stream().map(this::mapToArtist).toList().get(0);
+    }
+
+    public Collection<NapsterArtistCardDto> getTopArtists(int limit) {
         String url = "http://api.napster.com/v2.2/artists/top?apikey=" + apiKey + "&catalog=UK&limit=" + limit;
         var result = restTemplate.getForObject(url, NapsterArtistResponse.class);
 
-        return result.getArtists().stream().map(this::mapToArtist).collect(Collectors.toSet());
+        return result.getArtists().stream().map(this::mapToNapsterArtistCardDto).collect(Collectors.toSet());
     }
 
     private Artist mapToArtist(NapsterArtist napsterArtist) {
+        String resolution = "/images/633x422.jpg";
         var result = new Artist();
         result.setName(napsterArtist.getName());
         result.setGenre("UNKOWN");
         result.setId(napsterArtist.getId());
-        String image = getImage(napsterArtist.getId());
+        String image = createImageUrl(napsterArtist.getId(), resolution);
         result.setImage(image);
         result.setBlurbs(napsterArtist.getBlurbs());
         result.setType(napsterArtist.getType());
@@ -39,28 +45,17 @@ public class ArtistService {
     }
 
     private NapsterArtistCardDto mapToNapsterArtistCardDto(NapsterArtist napsterArtist) {
+        String resolution = "/images/356x237.jpg";
         var card = new NapsterArtistCardDto();
         card.setName(napsterArtist.getName());
         card.setId(napsterArtist.getId());
-        String image = getImage(napsterArtist.getId());
+        String image = createImageUrl(napsterArtist.getId(), resolution);
         card.setImage(image);
         card.setType(napsterArtist.getType());
         return card;
     }
 
-    public String getImage(String id) {
-        String image;
-        String url = "https://api.napster.com/v2.2/artists/" + id + "/images?apikey=" + apiKey + "&limit=1";
-        var result = restTemplate.getForEntity(url, JsonNode.class);
-        image = String.valueOf(result.getBody().findValue("url"));
-        image = image.substring(0, image.length() - 1);
-        image = image.substring(1);
-        return image+"?apikey="+apiKey;
-    }
-
-    public Artist getArtist(String id) {
-        String url = "https://api.napster.com/v2.2/artists/"+id+"?apikey="+apiKey;
-        var result = restTemplate.getForObject(url, NapsterArtistResponse.class);
-        return result.getArtists().stream().map(this::mapToArtist).toList().get(0);
+    public String createImageUrl(String artistId, String resolution) {
+        return "https://api.napster.com/imageserver/v2/artists/" + artistId + resolution;
     }
 }
