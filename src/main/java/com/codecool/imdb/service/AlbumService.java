c741063b98d37c90341521ad6393b4b697ad6657
nelli <csnelly90@gmail.com>
2022-09-06 14:24:43 +0200

    move RestTemplate to field in AlbumService

--- src/main/java/com/codecool/imdb/service/AlbumService.java
+++ src/main/java/com/codecool/imdb/service/AlbumService.java
@@ -1,7 +1,14 @@
 package com.codecool.imdb.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class AlbumService {
+
+    @Value("${api.key}")
+    private String apiKey;
+    private final RestTemplate restTemplate = new RestTemplate();
+
 }
