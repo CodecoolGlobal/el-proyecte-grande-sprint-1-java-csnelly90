2621260fc289745ef130544271a73f7a559e2e49
nelli <csnelly90@gmail.com>
2022-09-06 14:08:32 +0200

    remove unused imports from ArtistApiController

--- src/main/java/com/codecool/imdb/controller/ArtistApiController.java
+++ src/main/java/com/codecool/imdb/controller/ArtistApiController.java
@@ -4,12 +4,10 @@ import java.util.Collection;
 
 import com.codecool.imdb.domain.model.Artist;
 import com.codecool.imdb.service.ArtistService;
-import com.fasterxml.jackson.databind.JsonNode;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/artists")
