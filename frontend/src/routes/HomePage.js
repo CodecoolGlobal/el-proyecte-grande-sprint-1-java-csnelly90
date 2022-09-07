3ae15838292dccdc25644a07f0a57ad7425bc617
nelli <csnelly90@gmail.com>
2022-09-07 03:13:52 +0200

    refactor HomePage.js, add method to handle image selection mechanics in the trending sections

--- frontend/src/routes/HomePage.js
+++ frontend/src/routes/HomePage.js
@@ -1,34 +1,58 @@
 import React, {useState, useEffect} from "react";
 import "../HomePage.css"
 import {dataHandler} from "../data/DataHandler";
-import {useNavigate} from "react-router-dom";
+import TopTrending from "../components/TopTrending";
 
 function HomePage() {
     let [artists, setArtists] = useState(null);
     let [albums, setAlbums] = useState(null);
-
-    const navigate = useNavigate();
+    let [songs, setSongs] = useState(null);
 
     useEffect(() => {
         async function getData() {
-            let response = await dataHandler.apiGet("/api/artists");
-            setArtists(response);
+            let artistResponse = await dataHandler.apiGet("/api/artists");
+            setArtists(artistResponse);
         }
+
         getData();
     }, []);
 
+    const changeCardOrder = function (arrayType, id) {
+        if (arrayType === "artists") {
+            const index = artists.map(object => object.id).indexOf(id);
+            const artistsCopy = artists.slice();
+            artistsCopy.unshift(artistsCopy.splice(index, 1)[0]);
+            setArtists(artistsCopy);
+        } else if (arrayType === "albums") {
+            const index = albums.map(object => object.id).indexOf(id);
+            const albumsCopy = albums.slice();
+            albumsCopy.unshift(albumsCopy.splice(index, 1)[0]);
+            setArtists(albumsCopy);
+        } else if (arrayType === "songs") {
+            const index = songs.map(object => object.id).indexOf(id);
+            const songsCopy = songs.slice();
+            songsCopy.unshift(songsCopy.splice(index, 1)[0]);
+            setArtists(songsCopy);
+        }
+    }
 
-    return (
-        <div className="top-artists">
-            {artists == null ? (<h1>LOADING...</h1>):
-                artists.map(({ name, id, image }) => (
-                    <div className="artist" key={id}>
-                        <img src={image} alt="" onClick={() => navigate("/artists/"+id)}/>
-                        <h3 className="artist-name">{name}</h3>
-                    </div>
-                ))}
-        </div>
-    );
+    // TODO: also check albums and songs for null value when they are implemented
+    if (artists == null) {
+        return (
+            <div className="trending-content-wrapper">
+                <div className="loading">
+                    <h1>LOADING...</h1>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="trending-content-wrapper">
+                <TopTrending trendingItems={artists} changeCardOrder={changeCardOrder} apiRouteOption="artists"/>
+            </div>
+        );
+    }
+
 }
 
 export default HomePage;
