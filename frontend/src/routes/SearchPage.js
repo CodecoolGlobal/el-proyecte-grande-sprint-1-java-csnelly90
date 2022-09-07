a761cc32784a4599c988cc68cc33776dd9a9434a
Nandorrr <87696275+Nandorrr@users.noreply.github.com>
2022-09-07 23:53:57 +0200

    add display of usersearch in the search result page

--- frontend/src/routes/SearchPage.js
+++ frontend/src/routes/SearchPage.js
@@ -1,14 +1,16 @@
 import {faSearch} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {useParams, useSearchParams} from "react-router-dom";
 
 
 function SearchPage(props) {
     let searchIcon = <FontAwesomeIcon icon={faSearch}/>;
+    const {type} = {type: useParams()}
 
     return(
         <section>
             <div className="searchedTitleHolder">
-                <p> <span>{searchIcon}</span>Your search result's on <h3>{props.searchPhrase.toUpperCase()}</h3></p>
+                {searchIcon}Your search result's on <h3>{type.type} {type.searchInput}</h3>
             </div>
             <div className="other-cards">
 
