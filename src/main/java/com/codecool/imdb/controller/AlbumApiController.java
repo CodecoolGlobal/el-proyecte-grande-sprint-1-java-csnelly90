01c8d3b3bd61fc7adfba6e6f0abea31994d5d0a9
eagle <sasbalazs05@gmail.com>
2022-09-07 10:18:33 +0200

    implement method for get top ten albums route

--- src/main/java/com/codecool/imdb/controller/AlbumApiController.java
+++ src/main/java/com/codecool/imdb/controller/AlbumApiController.java
@@ -1,21 +1,18 @@
 package com.codecool.imdb.controller;
 
 import com.codecool.imdb.service.AlbumService;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.codecool.imdb.service.dtos.NapsterAlbum;
+import com.codecool.imdb.service.dtos.response.NapsterAlbumCardDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/api/albums")
 public class AlbumApiController {
-
-    @Value("${api.key}")
-    private String apiKey;
 
     private AlbumService albumService;
 
@@ -25,10 +22,8 @@ public class AlbumApiController {
     }
 
     @GetMapping
-    public JsonNode getTopTenAlbums(@RequestParam("genre") String genre) {
-        String url = "http://api.napster.com/v2.2/albums/top?apikey=" + apiKey + "&catalog=ENG&limit=10&genre=" + genre;
-        RestTemplate restTemplate = new RestTemplate();
-        JsonNode result = restTemplate.getForObject(url, JsonNode.class);
-        return result;
+    public Collection<NapsterAlbumCardDto> getTopTenAlbums() {
+        int limit = 10;
+        return albumService.getTopAlbums(limit);
     }
 }
