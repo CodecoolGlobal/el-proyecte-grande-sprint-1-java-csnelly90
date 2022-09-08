d7128becc0b2a7bb7f62f91ec788a491d115480d
eagle <sasbalazs05@gmail.com>
2022-09-08 09:54:14 +0200

    implement api request method

--- src/main/java/com/codecool/imdb/service/SongService.java
+++ src/main/java/com/codecool/imdb/service/SongService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.lang.reflect.Type;
+import java.util.Collection;
 import java.util.List;
 
 @Service
@@ -19,6 +20,20 @@ public class SongService {
     @Value("${api.key}")
     private String apiKey;
     private final RestTemplate restTemplate = new RestTemplate();
+
+    public Collection<NapsterSong> getTopSongs(int limit) throws JsonProcessingException {
+        String url = "https://api.napster.com/v2.2/tracks/top?apikey=" + apiKey + "&catalog=UK&limit=" + limit + "&range=week";
+        var resultData = restTemplate.getForObject(url, JsonNode.class);
+        JsonNode node = resultData.get("tracks");
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper.readValue(node.toString(), new TypeReference<List<NapsterSong>>() {
+            @Override
+            public Type getType() {
+                return super.getType();
+            }
+        });
+    }
 
     protected List<?> getUserCustomSongSearch(String userInput) throws JsonProcessingException {
         String url = "https://api.napster.com/v2.2/search/verbose?apikey=" + apiKey + "&query=" + userInput + "&type=track";
