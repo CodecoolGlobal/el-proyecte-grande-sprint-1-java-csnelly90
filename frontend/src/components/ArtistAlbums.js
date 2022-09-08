74b0648334333b950e27812d5708be8ebf3bc137
kornelIlles <kornelIlles10@gmail.com>
2022-09-08 09:58:46 +0200

    create ArtistAlbums component

---
+++ frontend/src/components/ArtistAlbums.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../ArtistPage.css"
+import CardView from "./CardView";
+import {useNavigate} from "react-router-dom";
+
+
+function ArtistAlbums(props) {
+    console.log(props.albumItems)
+    const navigate = useNavigate();
+
+    return (
+        <div className="card-wrapper">
+            {props.albumItems == null ? (<h1>LOADING ALBUMS...</h1>) :
+                (props.albumItems.map((item) =>(
+                        <CardView item={item}
+                                  key={item.id}
+                                  cardType="other"
+                                  apiOption={props.apiRouteOption}
+                                  handleClick={() => navigate(`/${props.apiRouteOption}/` + item.id)}/>
+                    ))
+                )}
+        </div>
+    )
+}
+
+export default ArtistAlbums;
+
