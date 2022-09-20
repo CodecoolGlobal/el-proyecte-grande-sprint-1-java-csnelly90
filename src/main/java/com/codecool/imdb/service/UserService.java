d1484c7fa1038c2e09b09efb630c7dd226107c94
nelli <csnelly90@gmail.com>
2022-09-20 15:34:47 +0200

    implement UserDetailsService in UserService, override loadUserByUsername method

--- src/main/java/com/codecool/imdb/service/UserService.java
+++ src/main/java/com/codecool/imdb/service/UserService.java
@@ -1,23 +1,51 @@
 package com.codecool.imdb.service;
 
 import com.codecool.imdb.data.repositories.UserRepository;
-import com.codecool.imdb.domain.entities.User;
+import com.codecool.imdb.domain.entities.AppUser;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Optional;
 
+@Slf4j
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public Optional<User> getUserById(Long id) {
+    public Optional<AppUser> getUserById(Long id) {
         return userRepository.findById(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<AppUser> user = userRepository.findByUsername(username);
+
+        if (user.isEmpty()) {
+            log.error("No user found in database by the following username: {}", username);
+            throw new UsernameNotFoundException("User not found in database.");
+        } else {
+            log.info("Loading user from database. Name: {}", username);
+        }
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        user.get().getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName()))
+        );
+
+        return new User(user.get().getUsername(), user.get().getPassword(), authorities);
     }
 }
