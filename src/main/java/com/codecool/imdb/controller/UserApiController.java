6ca0b9b7f18a1bc4b641b4f515318e11aa4c72e6
nelli <csnelly90@gmail.com>
2022-09-23 09:13:22 +0200

    add new api routes for getting current user's data and user data based on username

--- src/main/java/com/codecool/imdb/controller/UserApiController.java
+++ src/main/java/com/codecool/imdb/controller/UserApiController.java
@@ -3,6 +3,9 @@ package com.codecool.imdb.controller;
 import com.codecool.imdb.domain.entities.AppUser;
 import com.codecool.imdb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +28,19 @@ public class UserApiController {
     public AppUser getUserById(@PathVariable("userId") Long id) {
         Optional<AppUser> user = userService.getUserById(id);
         return user.orElse(null);
+    }
+
+    @GetMapping("/me")
+    @PreAuthorize("hasRole('USER')")
+    public AppUser getCurrentUser(@CurrentSecurityContext(expression="authentication?.name")
+                                      String username) {
+       return userService.getUserByUsername(username)
+               .orElseThrow(() -> new UsernameNotFoundException("There is no user with the following username: " + username));
+    }
+
+    @GetMapping("/{username}")
+    public AppUser getUserProfile(@PathVariable(value = "username") String username) {
+        return userService.getUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("There is no user with the following username: " + username));
     }
 }
