e15de0df8ee73bc8d71818c4a2b11cc0d6bb62a9
Nandorrr <87696275+Nandorrr@users.noreply.github.com>
2022-10-05 16:32:18 +0200

    add parameter to findCommentByTargetId method in CommentRepository.java

--- src/main/java/com/codecool/imdb/data/repositories/CommentRepository.java
+++ src/main/java/com/codecool/imdb/data/repositories/CommentRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByCommentTargetId(String targetId);
 
-    List<Comment> findByCommentTargetId();
 }
