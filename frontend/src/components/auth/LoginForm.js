3fb21ce161117779e8ac94463410bb08572494b5
nelli <csnelly90@gmail.com>
2022-10-05 21:35:20 +0200

    add error message to login page in case of unsuccessful login attempt

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -5,10 +5,11 @@ import {useNavigate} from "react-router-dom";
 function LoginForm() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [errorMsg, setErrorMsg] = useState("");
     const auth = useAuth();
     const navigate = useNavigate();
 
-    const handleSubmit = (event, username, password) => {
+    const handleSubmit = async (event, username, password) => {
         event.preventDefault();
 
         const payload = {
@@ -16,20 +17,30 @@ function LoginForm() {
             "password": password
         }
 
-        auth.login(payload);
-        navigate("/");
+        await auth.login(payload);
+
+        // TODO: fix redirect when logging in after a failed login attempt
+        if (auth.user) {
+            setErrorMsg("");
+            navigate("/");
+        } else {
+            setErrorMsg("Invalid username or password!");
+        }
     }
 
     return (
-        <form onSubmit={(event) => handleSubmit(event, username, password)} className="login-form">
-            <div><label htmlFor="username-field">USERNAME{" "}</label></div>
-            <input type="text" value={username} id="username-field" autoComplete="on" placeholder="Enter a username"
-                   required onChange={(e) => setUsername(e.target.value)}/>
-            <div><label htmlFor="pwd-field">PASSWORD{" "}</label></div>
-            <input type="password" value={password} id="pwd-field" placeholder="Enter a password"
-                   required onChange={(e) => setPassword(e.target.value)}/>
-            <button>Login</button>
-        </form>
+        <div id="login-signup-container">
+            <p className={errorMsg ? "error-msg": "offscreen"} aria-live="assertive">{errorMsg}</p>
+            <form onSubmit={(event) => handleSubmit(event, username, password)} className="login-form">
+                <div><label htmlFor="username-field">USERNAME{" "}</label></div>
+                <input type="text" value={username} id="username-field" autoComplete="on" placeholder="Enter a username"
+                       required onChange={(e) => setUsername(e.target.value)}/>
+                <div><label htmlFor="pwd-field">PASSWORD{" "}</label></div>
+                <input type="password" value={password} id="pwd-field" placeholder="Enter a password"
+                       required onChange={(e) => setPassword(e.target.value)}/>
+                <button>Login</button>
+            </form>
+        </div>
     );
 }
 
