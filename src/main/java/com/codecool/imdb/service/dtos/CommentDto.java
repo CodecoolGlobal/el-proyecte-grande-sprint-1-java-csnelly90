f28050f62b95e0da7353ae734c131a35bb918b4f
Nandorrr <87696275+Nandorrr@users.noreply.github.com>
2022-10-06 12:12:17 +0200

    add annotations related to constructor in CommentDto.java and fix fieldname

--- src/main/java/com/codecool/imdb/service/dtos/CommentDto.java
+++ src/main/java/com/codecool/imdb/service/dtos/CommentDto.java
@@ -1,14 +1,14 @@
 package com.codecool.imdb.service.dtos;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Builder
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommentDto {
-    private String userName;
+    private String username;
     private String text;
     private String commentTargetId;
 }
