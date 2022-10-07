23db16421b9ea3b7ae970530ad80d04391b2e95b
eagle <sasbalazs05@gmail.com>
2022-10-07 08:32:12 +0200

    make like and dislike routes to return response

--- src/main/java/com/codecool/imdb/controller/LikeController.java
+++ src/main/java/com/codecool/imdb/controller/LikeController.java
@@ -2,13 +2,17 @@ package com.codecool.imdb.controller;
 
 import com.codecool.imdb.domain.model.Item;
 import com.codecool.imdb.payload.request.LikeRequest;
+import com.codecool.imdb.payload.response.MessageResponse;
 import com.codecool.imdb.service.LikeService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Collection;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/likes")
 public class LikeController {
@@ -20,13 +24,15 @@ public class LikeController {
     }
 
     @PostMapping("/like")
-    public void likeItem(@RequestBody LikeRequest likeRequest) {
+    public ResponseEntity<?> likeItem(@Valid @RequestBody LikeRequest likeRequest) {
         likeService.saveLike(likeRequest.getUserName(), likeRequest.getLikedItem());
+        return ResponseEntity.ok(new MessageResponse("Like addition was successful"));
     }
 
-    @DeleteMapping("/dislike")
-    public void disLikeItem(@RequestBody LikeRequest likeRequest) {
+    @PostMapping("/dislike")
+    public ResponseEntity<?> disLikeItem(@RequestBody LikeRequest likeRequest) {
         likeService.deleteLike(likeRequest.getUserName(), likeRequest.getLikedItem());
+        return ResponseEntity.ok(new MessageResponse("Like deletion was successful"));
     }
 
     @GetMapping("/get-all")
