34a57242d404467bbd056fc79d06598f200b6a4e
eagle <sasbalazs05@gmail.com>
2022-10-07 08:35:13 +0200

    add NotBlank annotations to fields

--- src/main/java/com/codecool/imdb/payload/request/LikeRequest.java
+++ src/main/java/com/codecool/imdb/payload/request/LikeRequest.java
@@ -5,12 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class LikeRequest {
-
+    @NotBlank
     String userName;
+    @NotBlank
     String likedItem;
 }
