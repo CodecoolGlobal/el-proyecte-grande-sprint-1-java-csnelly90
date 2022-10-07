36f74e66d47f073cbabd215216074cad62b50059
eagle <sasbalazs05@gmail.com>
2022-10-07 08:33:01 +0200

    delete method not working yet

--- src/main/java/com/codecool/imdb/data/repositories/LikeRepository.java
+++ src/main/java/com/codecool/imdb/data/repositories/LikeRepository.java
@@ -3,6 +3,7 @@ package com.codecool.imdb.data.repositories;
 import com.codecool.imdb.domain.entities.AppUser;
 import com.codecool.imdb.domain.entities.Likes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -16,4 +17,8 @@ public interface LikeRepository extends JpaRepository<Likes, Long> {
     Likes findByItemId(String itemId);
 
     int getCountByItemId(String ItemId);
+
+    @Modifying
+    @Query("delete from Likes l where l.itemId = :itemId and l.user.id = :user")
+    void deleteByItemIdAndUserId(@Param("itemId") String itemId, @Param("user") Long userId);
 }
