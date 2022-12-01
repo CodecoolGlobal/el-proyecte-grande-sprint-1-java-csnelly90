30443adfcfb7d6221bd2cdb3fac4235f869152c1
eagle <sasbalazs05@gmail.com>
2022-12-01 09:50:42 +0100

    fetch liked items by user

--- frontend/src/components/UserLikes.js
+++ frontend/src/components/UserLikes.js
@@ -1,15 +1,32 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import "../UserPage.css"
 import UserLikesTab from "./UserLikesTab";
 import UserLikesContent from "./UserLikesContent";
+import {dataHandler} from "../data/DataHandler";
+import {useAuth} from "../security/auth";
 
 function UserLikes() {
-    const likedArtists = ['art.146985808', 'art.12313694', 'art.15237004'];
     const [toggleState, setToggleState] = useState(1);
+    let [likedItems, setLikedItems] = useState(null);
+    const auth = useAuth();
 
     const toggleTab = (index) => {
         setToggleState(index);
     };
+
+    useEffect(() => {
+        async function getData() {
+            try {
+                if (auth.user) {
+                    let items = await dataHandler.apiGet(`/api/likes/get-all?userName=${auth.user.username}`);
+                    setLikedItems(items);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getData();
+    }, []);
 
     return (
         <div id="user-likes-container">
@@ -22,7 +39,7 @@ function UserLikes() {
                 toggleState={toggleState}
                 setToggleState={setToggleState}
                 toggleTab={toggleTab}
-                likedArtists={likedArtists}
+                likedItems={likedItems}
             />
         </div>
     );
